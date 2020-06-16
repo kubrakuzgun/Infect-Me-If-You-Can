3d7b88318c78cc882169b587c38a2ca2a53d5b3e
Kübra Kuzgun <kubrakuzgun@users.noreply.github.com>
2020-06-16 16:35:45 +0300

    medshot added

---
+++ Assets/Scripts/FPS/MedicineShot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicineShot : MonoBehaviour
+{
+    public GameObject pillprefab;
+    public Transform gunbarrel;
+    public float forceamount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject pill = Instantiate(pillprefab, gunbarrel.position, Quaternion.identity);
+            Quaternion initialRot = pillprefab.transform.rotation;
+
+            pill.transform.rotation = initialRot;
+
+            // syringe.GetComponent<Rigidbody>().AddForce(gunbarrel.forward * forceamount, ForceMode.VelocityChange);
+            pill.GetComponent<Rigidbody>().AddForce(gunbarrel.up * forceamount);
+        }
+
+    }
+}
