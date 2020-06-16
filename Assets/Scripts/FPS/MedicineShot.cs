ca589c4aab6d95fe6c8a178e02a58f26686b068a
Kübra Kuzgun <kubrakuzgun@users.noreply.github.com>
2020-06-16 17:43:16 +0300

    med shot fix5

--- Assets/Scripts/FPS/MedicineShot.cs
+++ Assets/Scripts/FPS/MedicineShot.cs
@@ -27,6 +27,7 @@ public class MedicineShot : MonoBehaviour
        
             // syringe.GetComponent<Rigidbody>().AddForce(gunbarrel.forward * forceamount, ForceMode.VelocityChange);
             pill.GetComponent<Rigidbody>().AddForce(gunbarrel.up * forceamount);
+            pill.GetComponent<Rigidbody>().AddForce(gunbarrel.forward * forceamount/2);
         }
 
     }
