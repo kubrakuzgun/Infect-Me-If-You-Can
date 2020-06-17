224276ddd2a6725a2865cf072623125b66282557
Kübra Kuzgun <kubrakuzgun@users.noreply.github.com>
2020-06-17 19:02:54 +0300

    pause menu added

---
+++ Assets/Scripts/Other/PauseMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    private static bool gameIsPaused;
+    public GameObject pausemenu_ui;
+
+    void Start()
+    {
+        pausemenu_ui.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            pausemenu_ui.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausemenu_ui.SetActive(false);
+        }
+    }
+}
