ece0218fe19f5a4b388b00fe0dacbdf8e38d2111
Kübra Kuzgun <kubrakuzgun@users.noreply.github.com>
2020-06-17 17:15:16 +0300

    ls added

---
+++ Assets/Scripts/FPS/LiquidSanitizerConroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidSanitizerConroller : MonoBehaviour
+{
+    public GameObject liquid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        liquid.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            liquid.SetActive(true);
+        }
+        else 
+        {
+            liquid.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/FPS/SteamSanitizerConroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamSanitizerConroller : MonoBehaviour
+{
+    public GameObject steam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        steam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            steam.SetActive(true);
+        }        
+        else if (Input.GetMouseButtonUp(0))
+        {
+            steam.SetActive(false);
+        }
+    }
+}
