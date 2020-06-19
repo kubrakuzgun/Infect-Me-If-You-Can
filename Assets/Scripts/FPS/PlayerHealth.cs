1446eb718818904835e44db3ab451d51749269a0
Kübra Kuzgun <kubrakuzgun@users.noreply.github.com>
2020-06-19 22:59:48 +0300

    gameover panel added

--- Assets/Scripts/FPS/PlayerHealth.cs
+++ Assets/Scripts/FPS/PlayerHealth.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public float health=100;
-    private GameObject obj;
+    public GameObject gameover_panel, music;
     // Start is called before the first frame update
     void Start()
     {
+        gameover_panel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,6 +18,11 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             Debug.LogWarning("CoffinDance.mp3");
+            gameover_panel.SetActive(true);
+            music.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0f;
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -25,18 +31,18 @@ public class PlayerHealth : MonoBehaviour
         {
             if (other.GetComponent<InfectedPeopleHealer>().infected == true)
             {
-                if (health > 30)
+                if (health > 25)
                 {
-                    health -= 30;
+                    health -= 25;
                     if (health <= 10)
                     {
                         Debug.LogWarning("KORONOOOO!");
                     }
                 }
-            }
-            else
-            {
-                health = 0;
+                else
+                {
+                    health = 0;
+                }
             }
 
         }
@@ -45,29 +51,28 @@ public class PlayerHealth : MonoBehaviour
         {
             if (other.GetComponent<StandingPeopleHealer>().infected == true)
             {
-                if (health > 30)
+                if (health > 25)
                 {
-                    health -= 30;
+                    health -= 25;
                     if (health <= 10)
                     {
                         Debug.LogWarning("KORONOOOO!");
                     }
                 }
+                else
+                {
+                    health = 0;
+                }
             }
-            else
-            {
-                health = 0;
-            }
-
         }
 
 
 
         if (other.tag == "SurfaceVirus")
         {
-            if (health - 10 >= 0)
+            if (health - 5 >= 0)
             {
-                health -= 10;
+                health -= 5;
                 if (health <= 10)
                 {
                     Debug.LogWarning("KORONALANDIN!");
